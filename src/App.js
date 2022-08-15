1adf0613cfd8e96f9d438f4ab0fa79e1d40739e7
Natthawee Koengfak <Natthawee.Koengfak@agoda.com>
2022-08-15 13:19:52 +0700

    react init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+};
+
+export default App;
